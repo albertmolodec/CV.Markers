5e72485e8ae8169a83063432869a2421b32f74ec
Ksenia Kataeva <ksekataeva@yandex.ru>
2018-11-19 20:43:59 +0500

    Detected markers

--- StickersRecognition.py
+++ StickersRecognition.py
@@ -1,37 +1,45 @@
 import cv2
 import numpy as np
 
-image = cv2.imread("vlad3.jpg")
-image = cv2.resize(image,(1280,640))
+def getKey(item):
+    return item[0]
+
+image = cv2.imread("input/markered/box_right.jpg")
+image = cv2.resize(image,(1920,1080))
 
 #Normalize
 imageHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-# тут определить промежуток цвета в HSV
-lower_blue = np.array([90,50,50])
-upper_blue = np.array([140,255,255])
+hsv_min = np.array([26,210,231])
+hsv_max = np.array([255,255,255])
 
-mask = cv2.inRange(imageHSV, lower_blue, upper_blue)
+mask = cv2.inRange(imageHSV, hsv_min, hsv_max)
 kernel = np.ones((3,3),np.uint8)
 opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 
 (_, contours, hierarchy) = cv2.findContours(opening.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-# print(contours)
 ref = np.zeros_like(opening)
-cv2.drawContours(ref, np.asarray(contours), -1, 255, 1)
+cv2.drawContours(image, np.asarray(contours), -1, (0,0,255), 1)
+coords_array = []
 
-file = open("coords.txt", "a")
 for contour in contours:
     M = cv2.moments(contour)
-    centroid_x = int(M['m10']/M['m00'])
-    centroid_y = int(M['m01']/M['m00'])
-    file.write(str(centroid_x) + " " + str(centroid_y) + "\n")
-    cv2.circle(ref,(centroid_x, centroid_y), 2, 255, -1)
-file.close()    
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
+    coords_array.append([cx, cy])
+  
+font = cv2.FONT_HERSHEY_SIMPLEX 
+coords_array = sorted(coords_array, key = getKey)
+file = open("output/box/box_right.txt", "a")
+file.write(str(len(coords_array)) + "\n")
+for index, item in enumerate(coords_array):
+	cv2.putText(image, str(index + 1), (int(item[0] + 10), int(item[1]) + 10), font, 1, (0,242,255), 1, cv2.LINE_AA)
+	cv2.circle(image,(item[0], item[1]), 2, (0, 0, 255), 2)
+	file.write(str(item[0]) + " " + str(item[1]) + "\n")
+file.close()  
 
-
-cv2.imshow("image", imageHSV)
+cv2.imshow("image", image)
+cv2.imwrite("output/box/box_right_coords.jpg", image)
 cv2.imshow("mask", opening)
-cv2.imshow("contours", ref)
 k = cv2.waitKey(0)    
 cv2.destroyAllWindows()
